ec67c816d7d2af3971e4541cc2fbd214deb477a1
Michael Updegraff <michael.updegraff@gmail.com>
2015-02-03 05:28:04 -0800

    Initial Commit

---
+++ StockPrices/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StockPrices
+//
+//  Created by Michael Updegraff on 1/30/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+}
+
